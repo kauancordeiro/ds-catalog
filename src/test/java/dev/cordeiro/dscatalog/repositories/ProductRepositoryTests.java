45b86172141f24cfcef66cf887df5ba54b4b9863
Kauan <cvitorcordeiro@gmail.com>
2024-08-15 22:56:56 -0300

    delete product test

---
+++ src/test/java/dev/cordeiro/dscatalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,28 @@
+package dev.cordeiro.dscatalog.repositories;
+
+import dev.cordeiro.dscatalog.entities.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    public void deleteShouldDeleteObjectWhenIdExists(){
+        long id = 1L;
+
+        productRepository.deleteById(id);
+
+        Optional<Product> productResult = productRepository.findById(id);
+
+        Assertions.assertFalse(productResult.isPresent());
+    }
+
+}
